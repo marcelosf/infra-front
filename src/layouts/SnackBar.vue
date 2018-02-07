daa3c9d3cc139493ebecc89bd0eebb659b75f0b9
marcelosf <schneider.fei@gmail.com>
2018-02-07 17:20:11 -0200

    Creating a snackbar component

---
+++ src/layouts/SnackBar.vue
@@ -0,0 +1,51 @@
+<template>
+
+    <v-snackbar :timeout="timeout" :bottom="position" v-model="toggle">
+
+        {{ message }}
+
+        <v-btn flat color="pink" @click.native="snackbar.toggle = false">CLOSE</v-btn>
+
+    </v-snackbar>
+
+</template>
+
+<script>
+    export default {
+
+      props: ['message', 'value'],
+
+      data () {
+
+        return {
+
+          toggle: false,
+
+          timeout: parseInt(process.env.SNACKBAR_TIMEOUT),
+
+          position: 'bottom'
+
+        }
+
+      },
+
+      watch: {
+
+        value (value) {
+
+          this.toggle = value;
+
+        },
+
+        toggle (value) {
+
+          console.log(value);
+
+          this.$emit('input', value);
+
+        }
+
+      }
+
+    }
+</script>
