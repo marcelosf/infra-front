7fe288e64ac385a22f04256967e61dab7949ebd9
marcelosf <schneider.fei@gmail.com>
2018-02-27 13:44:55 -0300

    Changing the theme color and showing the app in fullscreen mode

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,7 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://localhost/api"',
+  API_URL: '"http://10.60.0.57/api"',
   REDIRECT: '"home"',
   SNACKBAR_TIMEOUT: '"5000"'
 })
